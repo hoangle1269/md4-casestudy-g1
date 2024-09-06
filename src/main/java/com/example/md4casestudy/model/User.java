f134a8e17f9dde39f8e3c4ed6567d269c15f62dc
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2024-09-06 13:42:35 +0700

    l1

--- src/main/java/com/example/md4casestudy/model/User.java
+++ src/main/java/com/example/md4casestudy/model/User.java
@@ -1,4 +1,26 @@
 package com.example.md4casestudy.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String fullName;
+    private Date dateOfBirth;
+    private String address;
+    private String identity;
+
 }
