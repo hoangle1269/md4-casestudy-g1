c3a414f705799714333005a964469a9a62334aea
Kiennguyen2406 <155146504+Kiennguyen2406@users.noreply.github.com>
2024-09-09 11:20:44 +0700

    sua lan cuoi model

--- src/main/java/com/example/md4casestudy/model/Student.java
+++ src/main/java/com/example/md4casestudy/model/Student.java
@@ -13,7 +13,7 @@ import java.util.Set;
 public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int studentId;
+    private Long studentId;
 
     private String fullName;
     private String email;
--- src/main/java/com/example/md4casestudy/model/Subject.java
+++ src/main/java/com/example/md4casestudy/model/Subject.java
@@ -10,7 +10,7 @@ import lombok.Data;
 public class Subject {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int subjectId;
+    private Long subjectId;
 
     private String subjectName;
 }
