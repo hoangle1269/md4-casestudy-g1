ee5330145d618b182ae522bf6ec79da1323dab83
Seitama179 <ngochd997@gmail.com>
2024-09-11 14:05:56 +0700

    Ngoc lam task 11

--- src/main/java/com/example/md4casestudy/controller/FeeController.java
+++ src/main/java/com/example/md4casestudy/controller/FeeController.java
@@ -1,7 +1,9 @@
 package com.example.md4casestudy.controller;
 
 import com.example.md4casestudy.model.Fee;
+import com.example.md4casestudy.model.Payment;
 import com.example.md4casestudy.service.fee.IFeeService;
+import com.example.md4casestudy.service.payment.IPaymentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,17 +18,20 @@ import java.util.Optional;
 @RequestMapping("/classes/students/fees")
 public class FeeController {
     private final IFeeService feeService;
-    public FeeController(IFeeService feeService) {
+    private final IPaymentService paymentService;
+    public FeeController(IFeeService feeService, IPaymentService paymentService) {
         this.feeService = feeService;
+        this.paymentService = paymentService;
     }
 
     @GetMapping("/{studentId}")
     public String getFeeStatus(Model model, @PathVariable("studentId") Long studentId) {
         List<Fee> fees = feeService.findByStudent(studentId);
-//        Optional<Fee> fees = feeService.findByStudentId(studentId);
-//        if (fees.isPresent()) {
-//        }
+        Iterable<Payment> payments = paymentService.findByStudentId(studentId);
         model.addAttribute("fees", fees);
+        model.addAttribute("payments", payments);
         return "fee/view";
     }
+
+
 }
--- src/main/java/com/example/md4casestudy/model/Payment.java
+++ src/main/java/com/example/md4casestudy/model/Payment.java
@@ -18,4 +18,46 @@ public class Payment {
 
     @ManyToOne
     private Student student;
+
+    public Payment() {
+    }
+
+    public Payment(Long id, LocalDate paymentDate, Double paymentAmount, Student student) {
+        this.id = id;
+        this.paymentDate = paymentDate;
+        this.paymentAmount = paymentAmount;
+        this.student = student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getPaymentDate() {
+        return paymentDate;
+    }
+
+    public void setPaymentDate(LocalDate paymentDate) {
+        this.paymentDate = paymentDate;
+    }
+
+    public Double getPaymentAmount() {
+        return paymentAmount;
+    }
+
+    public void setPaymentAmount(Double paymentAmount) {
+        this.paymentAmount = paymentAmount;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
 }
